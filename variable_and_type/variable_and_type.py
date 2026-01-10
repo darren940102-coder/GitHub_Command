93606d0b85730ea5eb74c6d97bb01f2cb53ac9cc
yao <darren940102@gmail.com>
2026-01-10 12:42:59 +0800

    First commit

---
+++ variable_and_type/variable_and_type.py
@@ -0,0 +1,16 @@
+age = 21
+#interger 整數
+a = 3
+#float 浮點數
+high = 178.2
+#string 字串
+name = "Darren"
+print(f"我的名字是 {name}", type(name))
+#boolean 布林值
+happy = True
+print(f"我的名字是{name}，今年{age}歲，身高{high}cm，大學{a}年級，是否快樂：{happy}")
+print('age', type(age))
+print('a', type(a))
+print('high', type(high))
+print('name', type(name))
+print('happy', type(happy))
